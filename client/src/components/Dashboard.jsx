fb79143902d88406847e272180d78d10d6dae3ae
Priyansh Singh <thetseffect@gmail.com>
2020-10-07 21:28:30 +0530

    Added

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -126,7 +126,7 @@ export default function Dasboard() {
   const onLoginClick = (e) => {
     setIsLogginIn(true);
     e.preventDefault();
-    window.open("http://twitt-dashboard.herokuapp.com/auth/twitter", "_self");
+    window.open("https://twitt-dashboard.herokuapp.com/auth/twitter", "_self");
     setIsLogginIn(false);
   };
 
